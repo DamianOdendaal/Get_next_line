7da1a8493f53db6b06ccb7e48517d79d7d4e1f8e
Damian ODENDAAL <dodendaa@c5r9s7.wethinkcode.co.za>
2019-07-02 15:47:39 +0200

    clean state

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dodendaa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/02 13:52:13 by dodendaa          #+#    #+#             */
+/*   Updated: 2019/07/02 15:47:31 by dodendaa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+
+int		get_next_line(int fd, char **line);
+
+#endif
