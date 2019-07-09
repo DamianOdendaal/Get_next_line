3cab6e3d91b245b0e537db6690227e7be86e9ce7
Damian ODENDAAL <dodendaa@c5r9s7.wethinkcode.co.za>
2019-07-09 13:26:32 +0200

    clean state but seg fault prob

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dodendaa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/02 14:00:39 by dodendaa          #+#    #+#             */
-/*   Updated: 2019/07/06 00:52:25 by dodendaa         ###   ########.fr       */
+/*   Updated: 2019/07/09 13:25:58 by dodendaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,28 @@
 
 static int		ft_newls(char **line, char **s)
 {
-	char		*str;
+	char	*str;
 	char		*temp;
 	int			len;
 
 	len = 0;
 	str = (char*)*s;
 	while ((str[len] != '\n') && (str[len] != '\0'))
-		len++;
-	if (str[len] == '\n')
 	{
-		*line = ft_strsub(str, str[0],len);
-		temp = ft_strdup(&str[len + 1]);
-		*s = temp;
-		if (s[0] == '\0')
+		if (str[len] == '\n')
+		{
+			*line = ft_strsub(str, str[0],len);
+			temp = ft_strdup(&str[len + 1]);
+			*s = temp;
+			if (s[0] == '\0')
+				ft_strdel(&*s);
+		}
+		if (str[len] == '\0')
+		{
+			*line = ft_strdup(str);
 			ft_strdel(s);
-	}
-	else if (str[len] == '\0')
-	{
-		*line = ft_strdup(str);
-		ft_strdel(s);
+		}
+		len++;
 	}
 	return (1);
 }
@@ -46,7 +48,7 @@ static int		ft_bread(const int fd, char **s, char *buff, int *readb)
 	while (*readb > 0)
 	{
 		buff[*readb] = '\0';
-		temp = ft_strjoin(*s, buff);
+		tvar = ft_strjoin(*s, buff);
 		ft_strdel(s);
 		*s = tvar;
 		if (ft_strchr(buff, '\n'))
@@ -62,11 +64,13 @@ int	get_next_line(const int fd, char **line)
 	char		*buff;
 	int			readb;
 
-	if(!line || read(fd, NULL, 0) == -1 )
+	if(!line || read(fd, NULL, 0) == -1)
 		return (-1);
+	buff = ft_memalloc(ft_strlen(buff));
+	readb = read(fd, buff, BUFF_SIZE);
 	if (readb > 0)
 		ft_bread(fd, &s, buff, &readb);
-	readb = read(fd, buff, BUFF_SIZE);
+
 	if (ft_strlen(s) == 0)
 	{
 		if (readb == 0)
@@ -74,5 +78,5 @@ int	get_next_line(const int fd, char **line)
 		return (0);
 	}
 	ft_strdel(&buff);
-	return (ft_newls(line, &s));
+		return (ft_newls(line, &s));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: dodendaa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/02 13:52:13 by dodendaa          #+#    #+#             */
-/*   Updated: 2019/07/06 06:27:21 by dodendaa         ###   ########.fr       */
+/*   Updated: 2019/07/09 11:17:03 by dodendaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 64
+# define BUFF_SIZE 1000000
 # include <fcntl.h>
 # include "libft/libft.h"
 
